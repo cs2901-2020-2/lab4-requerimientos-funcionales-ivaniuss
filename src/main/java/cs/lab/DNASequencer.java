f31891e2fb628fbea2714e9f156acfe3167a3ed5
ivaniuss <ivan.emi94@gmail.com>
2020-09-16 13:01:56 -0500

    done

--- src/main/java/cs/lab/DNASequencer.java
+++ src/main/java/cs/lab/DNASequencer.java
@@ -6,28 +6,32 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class  DNASequencer {
-
     static final Logger logger = Logger.getLogger(DNASequencer.class.getName());
     public DNASequencer() {
         logger.info("Starting sequencer...");
     }
-
     public String calculate(List<String> part){
         String minimunSequence = part.get(0);
-
-
+	int endIndex = 0;
         for(int i = 0; i < part.size()-1; i++){
-            String genoma = part.get(i);
-            Pattern word = Pattern.compile(genoma);
-            Matcher match = word.matcher(part.get(i+1));
-            while (!match.find()) {
-                genoma.substring(1);
-            }
-            int endIndx = match.end();
             
-            for(int j = endIndx; j < part.get(i+1).length(); j++)
+	    Boolean wordFounded = false;
+	    String genoma = part.get(i);
+            while (!wordFounded) {
+                Pattern word = Pattern.compile(genoma);
+		Matcher match = word.matcher(part.get(i+1));
+		if(match.find()){
+	        	wordFounded = true;
+			endIndex = match.end();
+		}else{
+			genoma = genoma.substring(1);
+		}
+	    }
+            for(int j = endIndex; j < part.get(i+1).length(); j++)
                 minimunSequence += part.get(i+1).charAt(j);  
         }
+		
         return minimunSequence;
+    
     }
 }
